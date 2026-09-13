722853ea77365defd52e48031d4c0cedef5934e6
Md Asif <asif212327@gmail.com>
2026-09-14 00:03:27 +0530

    Add files via upload

---
+++ Binary_Trees/Kth_Ancestor.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Kth_Ancestor {
+    static class Node{
+        int data; Node left,right;
+        Node(int data){
+            this.data=data; this.left=null; this.right=null;
+        }
+    }
+    public static int kthAncestor(Node root,int n,int k){
+        if(root==null)
+            return -1;
+        if(root.data==n)
+            return 0;
+        int leftDist=kthAncestor(root.left,n,k);
+        int rightDist=kthAncestor(root.right,n,k);
+        if(leftDist==-1 && rightDist==-1)
+            return -1;
+        int dist=Math.max(leftDist,rightDist)+1;
+        if(dist==k)
+            System.out.println(root.data);
+        return dist;
+    }
+    public static void main(String args[]){
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+        kthAncestor(root, 5, 1);
+    }
+}
